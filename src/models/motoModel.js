3995f83cc6685825b17a2118b9b8fb6f3fb2b2db
ViniciusMartins01 <vinicius.moliveira@sptech.school>
2026-05-23 18:19:25 -0300

    change files

--- src/models/motoModel.js
+++ src/models/motoModel.js
@@ -3,15 +3,15 @@ var database = require("../database/config");
 function listar() {
     console.log("ACESSEI O MOTO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function obter()");
         var instrucaoSql = `
-            select modelo, velocidadeMax from moto;
+            select modelo, velocidadeMax, peso from moto;
         `;
         console.log("Executando a instrução SQL: \n" + instrucaoSql);
         return database.executar(instrucaoSql);
 }
 
-function cadastrar(nome) {
+function cadastrar(modelo) {
     var instrucao = `
-        INSERT INTO moto (nome) VALUES ('${nome}');
+        INSERT INTO moto (modelo) VALUES ('${modelo}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
