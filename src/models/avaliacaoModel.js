f10bb580ee96af2de210482161c8e0705fb304dc
ViniciusMartins01 <vinicius.moliveira@sptech.school>
2026-06-02 03:58:20 -0300

    Create avaliacaoModel.js

---
+++ src/models/avaliacaoModel.js
@@ -0,0 +1,14 @@
+var database = require("../database/config")
+
+function inserir(nota, comentario) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nota, comentario);
+    var instrucaoSql = `
+        INSERT INTO avaliacao (nota, descricao) VALUES ('${nota}', '${comentario}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    inserir
+};
