da02333ae4696da311d2e5bab84999dfafed8960
ViniciusMartins01 <vinicius.moliveira@sptech.school>
2026-06-02 03:57:56 -0300

    Create avaliacaoController.js

---
+++ src/controllers/avaliacaoController.js
@@ -0,0 +1,34 @@
+var avaliacaoModel = require("../models/avaliacaoModel");
+
+function inserir(req, res) {
+    var nota = req.body.notaServer;
+    var comentario = req.body.comentarioServer;
+
+
+    if (nota == undefined) {
+        res.status(400).send("Seu nota está undefined!");
+    } else if (comentario == undefined) {
+        res.status(400).send("Seu comentario está undefined!");
+    } else {
+
+        avaliacaoModel.inserir(nota, comentario)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+module.exports = {
+    inserir
+}
